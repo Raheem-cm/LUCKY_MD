f56f5b4211b7b9b98594b386eff093502146bcfb
Raheem-cm <nyoniraheem@gmail.com>
2025-05-27 20:49:26 +0300

    Update life.js

--- luckycmd/life.js
+++ luckycmd/life.js
@@ -1,12 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const { ezra } = require("../fredi/ezra");
+const { ezra } = require("../Raheem-cm/18");
 ezra({ nomCom: "altest", reaction: "💐", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '🅼🆈 🅽🅰🅼🅴 🅸🆂 *🅻🆄🅲🅺🆈 🅼🅳* ғᴇʀʀᴀʀɪ 🚗 \n\n ' + "i'm a whatsapp bot multi-device created ";
+    let z = '🅼🆈 🅽🅰🅼🅴 🅸🆂 *RAHEEM-XMD* ғᴇʀʀᴀʀɪ 🚗 \n\n ' + "i'm a whatsapp bot multi-device created ";
     let d = ' by *ғʀᴇᴅɪᴇᴢʀᴀ255*';
     let varmess = z + d;
-    var img = 'https://files.catbox.moe/7irwqn.jpeg';
+    var img = 'https://files.catbox.moe/gahcu9.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
@@ -19,9 +19,9 @@ console.log("mon test");
 
 async function  execute  (origineMessage,zok) {
   console.log("Commande saisie !!!s")
-   let z ='Hi my name is *LUCKY MD* \n\n '+' I'm a Multi-device Whatsapp bot '
-      let d =' developed by *frediezra255*'
+   let z ='Hi my name is *RAHEEM-XMD* \n\n '+' I'm a Multi-device Whatsapp bot '
+      let d =' developed by *Raheem255*'
       let varmess=z+d
-      var img='https://files.catbox.moe/7irwqn.jpeg'
+      var img='https://files.catbox.moe/gahcu9.jpg'
 await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
 }  */ 
