93e322f62490fd5ff762434acd4bc0e98e04a72d
Raheem-cm <nyoniraheem@gmail.com>
2025-05-27 13:09:02 +0300

    Update channel.js

--- luckycmd/channel.js
+++ luckycmd/channel.js
@@ -1,13 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const { ezra } = require("../fredi/ezra");
+const { raheem } = require("../Raheem-cm/18");
 
 ezra({ nomCom: "channel", reaction: "💐", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
     let z = 'Salut je m\'appelle *LUCKY_MD* \n\n ' + 'je suis un bot Whatsapp Multi-appareil voici la chaîne';
     let d = ' developpé par *frediezra255*';
     let varmess = z + d;
-    var lien = 'https://whatsapp.com/channel/0029VaihcQv84Om8LP59fO3f';  // Remplacez cet URL par le lien que vous souhaitez envoyer
+    var lien = 'https://whatsapp.com/channel/0029VbAffhD2ZjChG9DX922r';  // Remplacez cet URL par le lien que vous souhaitez envoyer
     await zk.sendMessage(dest, { text: varmess + "\n" + lien });
 });
 
@@ -23,9 +23,9 @@ console.log("mon test");
 
 async function  execute  (origineMessage,zok) {
   console.log("Commande saisie !!!s")
-   let z ='Salut je m\'appelle *LUCKY-MD* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+   let z ='Salut je m\'appelle *RAHEEM-XMD* \n\n '+'je suis un bot Whatsapp Multi-appareil '
       let d =' developpé par *Fredi*'
       let varmess=z+d
-      var img='https://files.catbox.moe/xee8ol.jpg'
+      var img='https://files.catbox.moe/gahcu9.jpg'
 await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
 }  */ 
