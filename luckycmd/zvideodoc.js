b7a7745cde4aa32df8dc66db5d005c9c89e88978
Raheem-cm <nyoniraheem@gmail.com>
2025-05-28 02:10:48 +0300

    Update zvideodoc.js

--- luckycmd/zvideodoc.js
+++ luckycmd/zvideodoc.js
@@ -1,12 +1,12 @@
 
-const { ezra } = require("../fredi/ezra");
+const { raheem } = require("../Raheem-cm/18");
 const axios = require('axios');
 const ytSearch = require('yt-search');
 
 // Define the command with aliases
 ezra({
   nomCom: "videodoc",
-  aliases: ["musicvideodoc", "ytmp4doc", "luckyvideodoc", "mp4doc"],
+  aliases: ["musicvideodoc", "ytmp4doc", "Raheemvideodoc", "mp4doc"],
   categorie: "Search",
   reaction: "📺"
 }, async (dest, zk, commandOptions) => {
@@ -74,7 +74,7 @@ ezra({
           title: videoDetails.title,
           body: videoDetails.title,
           mediaType: 1,
-          sourceUrl: 'https://whatsapp.com/channel/0029VaihcQv84Om8LP59fO3f',
+          sourceUrl: 'https://whatsapp.com/channel/0029VbAffhD2ZjChG9DX922r',
           thumbnailUrl: firstVideo.thumbnail,
           renderLargerThumbnail: false,
           showAdAttribution: true,
