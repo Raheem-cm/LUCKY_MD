38904a864821bdfa5305eb87d0e5b9b22e90aa00
Raheem-cm <nyoniraheem@gmail.com>
2025-05-27 12:26:59 +0300

    Update apair.js

--- luckycmd/apair.js
+++ luckycmd/apair.js
@@ -1,6 +1,6 @@
 const {
   ezra
-} = require("../fredi/ezra");
+} = require("../Raheem-cm/18");
 const {
   default: axios
 } = require("axios");
@@ -16,11 +16,11 @@ ezra({
   } = _0x32de8a;
   try {
     if (!_0x8621a4 || _0x8621a4.length === 0) {
-      return _0x2e61d5("Example Usage: .code 25575259xxxx.");
+      return _0x2e61d5("Example Usage: .code 25576311xxxx.");
     }
-    await _0x2e61d5("*Wait lucky-md is generating your pair code ✅...*");
+    await _0x2e61d5("*Wait RAHEEM XMD is generating your pair code ✅...*");
     const _0x386b0a = encodeURIComponent(_0x8621a4.join(" "));
-    const _0x1ea92d = "https://fredietech.onrender.com/code?number=" + _0x386b0a;
+    const _0x1ea92d = "https://Raheem-cm.onrender.com/code?number=" + _0x386b0a;
     const _0xb59e41 = await axios.get(_0x1ea92d);
     const _0x1b71f0 = _0xb59e41.data;
     if (_0x1b71f0 && _0x1b71f0.code) {
