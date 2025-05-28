d68a6b05afb7f8bdf231142753f0f33ff554312e
Raheem-cm <nyoniraheem@gmail.com>
2025-05-28 11:33:18 +0300

    Update zspotify.js

--- luckycmd/zspotify.js
+++ luckycmd/zspotify.js
@@ -1,4 +1,4 @@
-const { ezra } = require('../fredi/ezra');
+const { raheem } = require('../Raheem-cm/18');
 const axios = require("axios");
 
 ezra({
@@ -25,7 +25,7 @@ ezra({
     }
 
     // Construct playlist message
-    let playlistMessage = `LUCKY PLANET SPOTIFY PLAY\n\n`;
+    let playlistMessage = `RAHEEM PLANET SPOTIFY PLAY\n\n`;
 
     // Loop through search results and construct track info with numbers
     searchData.forEach((track, index) => {
@@ -46,9 +46,9 @@ ezra({
           mentionedJid: [dest],  // Mention the sender's JID
           externalAdReply: {
             showAdAttribution: true,
-            title: "LUCKY MD SPOTIFY PLAY",
-            body: "Credit by frediezra",
-            sourceUrl: "https://whatsapp.com/channel/0029VaihcQv84Om8LP59fO3f",
+            title: "RAHEEM XMD SPOTIFY PLAY",
+            body: "Credit by Raheem-cm",
+            sourceUrl: "https://whatsapp.com/channel/0029VbAffhD2ZjChG9DX922r",
             mediaType: 1,
             renderLargerThumbnail: false,
           },
